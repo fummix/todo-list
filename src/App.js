04c57a30b3c182b68c0b3577cb901cda4ee2b5a0
funmilayo <fummix4real@gmail.com>
2024-03-07 11:01:29 +0100

    completed the design of todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+        <div className="content">
+          <h2>Add new Todo</h2>
+
+          <form action='' className="task-form">
+            <input type="text" placeholder="Todo Name" id="task-form-input" />
+            <input type="submit" value="Add" id="task-form-submit" />
+
+          </form>
+        </div>
+        <section>
+          <div className="main">
+            <h2>My todo list</h2>
+            <div id="task-list">
+              <div className="task">
+                <div className="container">
+                  <p>Reading Bible</p>
+
+                </div>
+                <div className="icons" >
+                  <button className="edit"  style={{"background-color": "blue" , color: "white", padding: "8px 16px", "border-radius": "8px"}}>Edit</button>
+                  <button className="delete"style={{"background-color": "blue",  color: "white", padding: "8px 16px", "border-radius": "8px" ,"margin-left": "5px"}}>Delete</button>
+                </div>
+
+              </div>
+              <div className="task">
+                <div className="container">
+                  <p>Reading Bible</p>
+
+                </div>
+                <div className="icons" >
+                  <button className="edit" style={{"background-color": "blue" , color: "white", padding: "8px 16px", "border-radius": "8px"}}>Edit</button>
+                  <button className="delete" style={{"background-color": "blue",  color: "white", padding: "8px 16px", "border-radius": "8px" ,"margin-left": "5px"}}>Delete</button>
+                </div>
+
+              </div>
+              <div className="task">
+                <div className="container">
+                  <p>Reading Bible</p>
+
+                </div>
+                <div className="icons" >
+                  <button className="edit" style={{"background-color": "blue" , color: "white", padding: "8px 16px", "border-radius": "8px"}}>Edit</button>
+                  <button className="delete"style={{"background-color": "blue",  color: "white", padding: "8px 16px", "border-radius": "8px" ,"margin-left": "5px"}}>Delete</button>
+                </div>
+
+              </div>
+              <div className="task">
+                <div className="container">
+                  <p>Cooking</p>
+
+                </div>
+                <div className="icons" >
+                  <button className="edit" style={{"background-color": "blue" , color: "white", padding: "8px 16px", "border-radius": "8px"}}>Edit</button>
+                  <button className="delete" style={{"background-color": "blue",  color: "white", padding: "8px 16px", "border-radius": "8px" ,"margin-left": "5px"}} >Delete</button>
+                </div>
+
+              </div>
+
+
+            </div>
+          </div>
+        </section>
+
+      </div>
+    
+
   );
 }
 
