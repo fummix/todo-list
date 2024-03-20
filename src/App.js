1c4cda5ce64a8cf9bc30d110b125c303105ea1cd
funmilayo <fummix4real@gmail.com>
2024-03-20 15:39:47 +0100

    Completed the design of the new todo

--- src/App.js
+++ src/App.js
@@ -3,140 +3,99 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const[todoList, setTodoList] = useState([])
-  const[todo, setTodo] = useState({todoName:'', id: todoList.length+1,status:false})
- //##################
- const showRecordInForm=(item)=>{
-setTodo(item)
- }
-//################Delete Record From todoList
-const deleteRecord=(id)=>{
-  console.log("We retrieve Id for deletion", id)
-  const filteredList = todoList.filter(existingTodo=> existingTodo.id!==id)
-  console.log(filteredList)
-  setTodoList(filteredList)
-  saveToLocalStorage()
-}
-//#################
+  const [todoList, setTodoList] = useState([])
+  const [todo, setTodo] = useState({ todoName: '', id: todoList.length + 1, status: false })
+  //##################
+  const showRecordInForm = (item) => {
+    setTodo(item)
+  }
+  //################Delete Record From todoList
+  const deleteRecord = (id) => {
+    console.log("We retrieve Id for deletion", id)
+    const filteredList = todoList.filter(existingTodo => existingTodo.id !== id)
+    console.log(filteredList)
+    saveToLocalStorage(filteredList)
+    setTodoList(filteredList)
+  }
+  //#################
   const handleChange = (e) => {
-    const { name, value} = e.target;
-     setTodo({
+    const { name, value } = e.target;
+    setTodo({
       ...todo,
       [name]: value
     });
   };
-  const handleCheckBoxChange = (e) => {
-    const { name, checked} = e.target;
-    console.log(name, checked)
-    setTodo({
-      ...todo,
-      [name]: checked
-    });
+  const handleCheckBoxChange = (e, item) => {
+    const { name, checked } = e.target;
+    console.log(name, checked, item)
+    if(checked){
+      showRecordInForm(item)
+    }
   };
-const saveToLocalStorage =()=>{ // method to save to local storage
-  const todoJson =JSON.stringify(todoList) // converted to json before saving
-localStorage.setItem("todo", todoJson); // save to local storage
-}
+  const saveToLocalStorage = (itemList) => { // method to save to local storage
+    const todoJson = JSON.stringify(itemList) // converted to json before saving
+    localStorage.setItem("todo", todoJson); // save to local storage
+  }
 
-const getFromLocalStorage=()=>{
-const itemFromStorage = localStorage.getItem("todo"); // retrieve from storage
-if(itemFromStorage){
-  const toObject = JSON.parse(itemFromStorage); // convert to normal object
-setTodoList(toObject);
-}
-}
-const AddToList=(event)=>{
-event.preventDefault();
-todoList.push(todo) // add todo to list
-saveToLocalStorage()
-setTodo({todoName:'',id: todoList.length+1, status: false})
+  const getFromLocalStorage = () => {
+    const itemFromStorage = localStorage.getItem("todo"); // retrieve from storage
+    if (itemFromStorage) {
+      const toObject = JSON.parse(itemFromStorage); // convert to normal object
+      setTodoList(toObject);
+    }
+  }
+  const AddToList = (event) => {
+    event.preventDefault();
+    if (todo.todoName.trim() === '') {
+      alert('Kindly input a todoname');
+      return;
+    }
+    todoList.push(todo) // add todo to list
+    saveToLocalStorage(todoList)
+    setTodo({ todoName: '', id: todoList.length + 1, status: false })
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getFromLocalStorage()
   }, []);
-  
+
   return (
     <div className="App">
-      
-        <div className="content">
-          <h2>Add new Todo</h2>
 
-          <form  className="task-form" onSubmit={AddToList}>
-          
-          
-            <input type="text" name="todoName" placeholder="Todo Name" id="task-form-input" value={todo.todoName}
-             onChange={handleChange}/>
-           
-            <button  type='submit' className="btn" >Add</button>
+      <div className="content">
+        <h2>Add new Todo</h2>
 
-          </form>
-        </div>
-        <section>
-          <div className="main">
-            <h2>My todo list</h2>
-            <div id="task-list">
-  {todoList.length > 0 ? (
-    todoList.map((item) => (
-      <div className="task" key={item.id}>
-        {/* <div className="container">
-          <p style={{textDecoration: item.status?'line-through':'none'}}>{item.todoName}</p>
-        </div> */}
-        {/* <div className="icons">
-                  <button onClick={()=>showRecordInForm(item)}
-            className="edit"
-            style={{
-              backgroundColor: "blue",
-              color: "white",
-              padding: "8px 16px",
-              borderRadius: "8px"
-            }}
-          >
-            Edit
-          </button>
-          <button onClick={()=> deleteRecord(item.id)}
-            className="delete"
-            style={{
-              backgroundColor: "blue",
-              color: "white",
-              padding: "8px 16px",
-              borderRadius: "8px",
-              marginLeft: "5px"
-            }}
-          >
-            Delete
-          </button>
-        </div> */}
-      </div>
-    ))
-  ) : (
-    // <p> No tasks available</p>
-    <div>
-        {/* <div className="container"> */}
-            <ul className="flex-container">
-                <li className="row">
-                    <input type="checkbox" className="sm1" /> 
-                    <span className="sm10">task1</span>
-                    <i className="material-icons icon">delete</i>
+        <form className="task-form" onSubmit={AddToList}>
+
+
+          <input type="text" name="todoName" placeholder="Todo Name" id="task-form-input" value={todo.todoName}
+            onChange={handleChange} />
+
+          <button type='submit' className="btn" >Add Item</button>
+
+        </form>
+        <section className='main'>
+
+
+          <ul className="flex-container" >
+            {
+              todoList.map((item) => (
+
+                <li className="row" key={item.id} >
+                  <input type="checkbox" className="sm1" id="myCheckbox" name="status" onChange={(event) => handleCheckBoxChange(event, item)} />
+                  <span className="sm10">{item.todoName}</span>
+                  <i className="material-icons icon" style={{ "marginRight": "-40px" }} onClick={() => deleteRecord(item.id)}>delete</i>
                 </li>
-                <li className="row">
-                    <input type="checkbox" className="sm1" /> 
-                    <span className="sm10">task2</span>
-                    <i className="material-icons icon">delete</i> 
-                </li>
-                
-            </ul>
-            
-        </div>
-    // </div>
-  )}
-</div>
+              )
+              )}
+          </ul>
 
-          </div>
         </section>
-
       </div>
-    
+
+
+    </div>
+
 
   );
 }
